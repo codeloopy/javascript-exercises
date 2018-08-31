de1c12ea31fcc39010986da2527853e843e11957
Pedro <codeloopy@gmail.com>
2018-08-31 15:36:18 -0400

    Finished SumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let total = 0;
 
+  while((typeof num1 == "number") && (typeof num2 == "number")){
+    if((num1 < 0) || (num2 < 0)){
+      return "ERROR";
+    } else if(num1 < num2) {
+      for(var i = num1; i <= num2; i++){
+        total += i;
+      }
+    } else {
+      for (var i = num2; i <= num1; i++) {
+        total += i;
+      }
+    }
+
+    return total;
+  }
+  return "ERROR";
 }
 
 module.exports = sumAll
