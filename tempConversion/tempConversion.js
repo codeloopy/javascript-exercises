2204a4c020937858b5b7bc6e9e1e9101d76762d6
Pedro <codeloopy@gmail.com>
2018-08-31 16:55:41 -0400

    Completed tempConversion)

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  var celsius = (fahrenheit - 32) * 5/9;
+  return celsius == 0 ? parseFloat(0) : parseFloat(celsius.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  var fahrenheit = parseFloat((9/5 * celsius) + 32);
+  return fahrenheit == 0 ? parseFloat(0) : parseFloat(fahrenheit.toFixed(1));
 }
 
 module.exports = {
