57a9fd822f02dc60063eb0d5571733bf6f6c7d03
Pedro <codeloopy@gmail.com>
2018-08-31 15:39:29 -0400

    Finished SumAll exercise, need more time for the removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removes) {
+  return array.filter(array => array != removes);
 }
 
 module.exports = removeFromArray
